eeee4593aa064470c7de1ca3dea4e2dc2c0193ed
Arthur-DevionFox <blackberserkfox@mail.ru>
2024-04-27 11:35:32 +0300

    Fix console.log() Created uniqueId

--- task_1/index.js
+++ task_1/index.js
@@ -2,8 +2,11 @@ import {encoded, translations} from './data.js'
 
 const translation = () => {
 
+    let uniqueId = []
+    let decoded = {}
+
     const decode = encoded.map((item, id) => {
-        let decoded = {}
+
 
 
         for (const key in item) {
@@ -11,35 +14,38 @@ const translation = () => {
                 switch (item[key]) {
                     case 'groupId': {
                         decoded[key] = item[key]
+                        break
                     }
                     case 'service': {
                         decoded[key] = item[key]
+                        break
                     }
                     case 'formatSize': {
                         decoded[key] = item[key]
+                        break
                     }
                     case 'ca': {
                         decoded[key] = item[key]
+                        break
                     }
                     default: {
                         decoded[key] = translations[item[key]] || item[key]
                     }
                 }
-                console.log(decoded)
+
             } else {
                 decoded[key] = item[key]
-                console.log(decoded)
             }
         }
+        uniqueId.push(item.groupId)
+        console.log(decoded)
     })
+
+    console.log(uniqueId)
 }
 
 translation()
 
 console.log("Let's rock")
 console.log("ROCK AND STONE!")
-//console.log(encoded, translations)
-
-
-
-// console.log(decoded)
+// console.log(encoded, translations)
